0b83b4fc4b352e75454ba6ebc9bd2c23d0953348
unknown <fegoworks@hotmail.com>
2020-03-27 22:31:27 +0100

    chore(utils): create helper functions to hash pass and generate user tokens

--- backend/src/helpers/utils.js
+++ backend/src/helpers/utils.js
@@ -1,3 +1,37 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+/**
+ * @param {string} password
+ * @return {string} hash
+ */
+export const hashPassword = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+/**
+ * @param {string} password
+ * @param {string} hashPwd
+ * @return {string} hash
+ */
+export const comparePassword = (password, hashPwd) => bcrypt.compareSync(password, hashPwd);
+
+/**
+ * @param {object} payload
+ * @param {string} tokenExpiryDate
+ * @param {string} secret
+ * @return {string} token
+ */
+export const generateToken = (
+  payload,
+  tokenExpiryDate = '1h',
+  secret = process.env.SECRET
+) => jwt.sign(
+  payload,
+  secret, {
+    expiresIn: tokenExpiryDate
+  }
+);
+
+
 export const handleSuccessResponse = (res, data, statusCode = 200) => res.status(statusCode).json({
   status: 'success',
   data,
